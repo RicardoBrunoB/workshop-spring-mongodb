c057ab39eb9de179381804d7fe9358d2fea526d7
Ricardo Bruno <ricardobrunob@gmail.com>
2022-05-18 18:03:56 -0300

    Search with many parameters

--- src/main/java/com/ricardobrunob/workshopmongo/services/PostService.java
+++ src/main/java/com/ricardobrunob/workshopmongo/services/PostService.java
@@ -1,12 +1,13 @@
 package com.ricardobrunob.workshopmongo.services;
 
+import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ricardobrunob.workshopmongo.domain.Post;
-import com.ricardobrunob.workshopmongo.domain.User;
 import com.ricardobrunob.workshopmongo.repository.PostRepository;
 import com.ricardobrunob.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -21,4 +22,12 @@ public class PostService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
+	public List<Post> findByTitle(String text) {
+		return repo.searchTitle(text);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repo.fullSearch(text, minDate, maxDate);
+	}
 }
